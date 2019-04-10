a6d9f423e9d70a2bc3b4b410f08f5ee037578f9c
Ben <koontz_ben@yahoo.com>
2019-04-10 12:55:57 -0700

    first commit

---
+++ Debug/DebugSession/DebugSession/TotalClass.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace DebugSession
+{
+	/// <summary>
+	/// Summary description for TotalClass.
+	/// </summary>
+	public class TotalClass
+	{
+		/// <summary>
+		/// Gets and Sets number of males
+		/// </summary>
+		public int Males
+		{
+			set{this.males = value;}
+			get{return this.males;}
+		}
+		private int males = 0;  // bpk
+
+		/// <summary>
+		/// Gets and Sets number of females
+		/// </summary>
+		public int Females
+		{
+			set{this.females = value;}
+			get{return this.females;}
+		}
+		private int females = 0; // bpk
+
+	}//end class
+}
